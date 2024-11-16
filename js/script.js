31499e1343179e3ddd2683666832704d9f5508eb
Erbol11 <perat5959@gemail.com>
2024-11-16 08:15:50 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const todoInput = document.getElementById('todoInput');
+const addTodoBtn = document.getElementById('addTodoBtn');
+const todoList = document.getElementById('todoList');
+
+function addTodo(){
+    const todoText = todoInput.value.trim();
+
+    if (todoText !== ''){
+        const todoItem = document.createElement('li');
+        todoItem.innerText = todoText;
+
+        const removeBtn = document.createElement('button');
+        removeBtn.innerText = 'Очуруу';
+        removeBtn.classList.add('remove-btn')
+
+        removeBtn.addEventListener('click', ()=>{
+            todoList.removeChild(todoItem);
+        })
+
+        todoList.appendChild(todoItem);
+        todoItem.appendChild(removeBtn);
+    }
+}
+
+addTodoBtn.addEventListener('click', addTodo)
